e01bdb045fbd38a9deb8e7fe5cd7206094dd246c
alice15311 <57412796+alice15311@users.noreply.github.com>
2021-02-27 16:56:59 -0500

    added methods for LEDSubsystem

--- src/main/java/frc/robot/subsystems/LEDSubsystem.java
+++ src/main/java/frc/robot/subsystems/LEDSubsystem.java
@@ -18,12 +18,44 @@ public class LEDSubsystem extends SubsystemBase {
     ledBuffer = new AddressableLEDBuffer(69);
     ledStrip.setLength(ledBuffer.getLength());
     
-    ledStrip.setData(ledBuffer);
-    ledStrip.start();
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    rainbow(0); //red
+    ledStrip.start();
   }
+
+  private void rainbow(int rainbowFirstPixelHue) {
+    // For every pixel
+    for (var i = 0; i < ledBuffer.getLength(); i++) {
+      // Calculate the hue - hue is easier for rainbows because the color
+      // shape is a circle so only one value needs to precess
+      final var hue = (rainbowFirstPixelHue + (i * 180 / ledBuffer.getLength())) % 180;
+      // Set the value
+      ledBuffer.setHSV(i, hue, 255, 128);
+    }
+    // Increase by to make the rainbow "move"
+    rainbowFirstPixelHue += 3;
+    // Check bounds
+    rainbowFirstPixelHue %= 180;
+    ledStrip.setData(ledBuffer);
+  }
+
+  public void setHSV(int hue, int saturation, int value){
+    for (var i = 0; i < ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the HSV values for red
+      ledBuffer.setHSV(i, hue, saturation, value);
+   }
+   ledStrip.setData(ledBuffer);
+  }
+  
+  public void setRGB(int red, int green, int cyan){
+    for (var i = 0; i < ledBuffer.getLength(); i++) {
+      // Sets the specified LED to the RGB values for red
+      ledBuffer.setRGB(i, red, green, cyan);
+   }
+   ledStrip.setData(ledBuffer);
+  }
+
 }
